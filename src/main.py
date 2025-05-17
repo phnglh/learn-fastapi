885160ba2434ed7a9a0d7141fabad6d3d4050e90
Le Hong Phong - Phong LH <phonghngle@gmail.com>
2025-05-17 23:02:46 +0700

    body update

--- src/main.py
+++ src/main.py
@@ -1,65 +1,35 @@
-# from typing import Union
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
 
-from fastapi import FastAPI, Query, APIRouter, Body
-from typing import Annotated, Set
-from pydantic import BaseModel, Field
 app = FastAPI()
 
-api_router = APIRouter(prefix="/api")
-
-
-fake_items_db=[{"item_name": "phong"},{"item_name": "huy"},{"item_name": "huyen"}]
 
 class Item(BaseModel):
-    name: str
-    description: str | None = Field(
-        default=None, title="The description of the item", max_length=300, examples=["A very nice Item"]
-    )
-    price: float
-    tax: float | None = None
-    tags: Set[str] = set()
-
-    model_config = {
-        "json_schema_extra": {
-            "examples": [
-                {
-                    "name": "Foo",
-                    "description": "A very nice Item",
-                    "price": 35.4,
-                    "tax": 3.2,
-                    "tags": ["iphone", "mobile"]
-                }
-            ]
-        }
-    }
-
-@api_router.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@api_router.get("/items")
-async def read_items(q: Annotated[list[str] | None, Query()] = None):
-    query_items = {"q": q}
-    return query_items
-
-@api_router.get("/items/{item_id}")
-def read_item(item_id: int, q: Annotated[str| None, Query(min_length=3, max_length=30)] = None):
-    if q:
-        return {"item_id": item_id, "q": q}
-    return {"item_id": item_id}
-
-@api_router.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
-    results = {"item_id": item_id, "item": item}
-    return results 
-
-@app.post("/no_embed")
-def no_embed(item: Item):
-    return item
-
-@app.post("/with_embed")
-def with_embed(item: Annotated[Item, Body(embed=True)]):
-    return item
+    name: str | None = None
+    description: str | None = None
+    price: float | None = None
+    tax: float = 10.5
+    tags: list[str] = []
 
 
-app.include_router(api_router)
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
+
+
+@app.get("/items/{item_id}", response_model=Item)
+async def read_item(item_id: str):
+    return items[item_id]
+
+
+@app.patch("/items/{item_id}", response_model=Item)
+async def update_item(item_id: str, item: Item):
+    stored_item_data = items[item_id]
+    stored_item_model = Item(**stored_item_data)
+    update_data = item.model_dump(exclude_unset=True)
+    updated_item = stored_item_model.model_copy(update=update_data)
+    items[item_id] = jsonable_encoder(updated_item)
+    return updated_item
